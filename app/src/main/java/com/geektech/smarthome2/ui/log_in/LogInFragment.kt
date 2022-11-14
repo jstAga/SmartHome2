8aef0784617562f4920e92043088614e90f7e367
Aktan <86038270+jstAga@users.noreply.github.com>
2022-11-14 13:50:28 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/smarthome2/ui/log_in/LogInFragment.kt
@@ -0,0 +1,38 @@
+package com.geektech.smarthome2.ui.log_in
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.geektech.smarthome2.R
+import com.geektech.smarthome2.databinding.FragmentLogInBinding
+
+
+class LogInFragment : Fragment() {
+    private lateinit var binding: FragmentLogInBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLogInBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener{
+            logIn()
+        }
+    }
+
+    private fun logIn() {
+        if (binding.etEmail.text.toString() == "admin" && binding.etPassword.text.toString() == "admin"){
+            findNavController().navigate(R.id.navigation_home)
+        }
+    }
+
+}
